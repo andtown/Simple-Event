a9fbca242cf8d1a76b40193e1f4952cef7b54238
andtown <anthonius.andreas@gmail.com>
2018-07-16 00:13:47 +0700

    files and directories structure establishment

---
+++ Simple-Event.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* Plugin Name: Simple Event
+* Plugin URI: 
+* Version: 0.0.1
+* Author: Andtown
+* Author URI: 
+* Description: 
+* License: MIT
+*/
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
