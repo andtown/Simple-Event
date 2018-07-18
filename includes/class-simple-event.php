7a1e7b4293aa178ad2eb7c7edeb27826993d4a35
andtown <anthonius.andreas@gmail.com>
2018-07-18 23:37:17 +0700

    metabox, jQuery datepicker, Google Maps and Google places autocomplete functionalities

--- includes/class-simple-event.php
+++ includes/class-simple-event.php
@@ -20,7 +20,7 @@ class Simple_Event {
 	 * @since 0.0.1
 	 */
 	private function create_event_post_type() {
-	    register_post_type('events', 
+	    register_post_type('event', 
 	        array(
 	        'labels' => array(
 	            'name' => __('Events', 'simple-event'),
@@ -49,30 +49,30 @@ class Simple_Event {
 	        ),
 	        'can_export' => true,
 	        'taxonomies' => array(
-	            'events_category',
-	            'events_tag'
+	            'event_category',
+	            'event_tag'
 	        ) 
 	    ));
 
 	    register_taxonomy(
-	        'events_category',
-	        'events',
+	        'event_category',
+	        'event',
 	        array(
 	            'label'              => 'Categories',
 	            'show_in_quick_edit' => true,
 	            'show_admin_column'  => true,
 	            'hierarchical'       => true,
-	            'query_var'          => 'events_category',
+	            'query_var'          => 'event_category',
 				'rewrite' => [
 					'hierarchical' => true,
-					'slug' => 'events-category',
+					'slug' => 'event-category',
 					'with_front' => true,
 					'ep_mask' => EP_NONE,	
 				]
 	        )
 	    );
 
-    	register_taxonomy( 'events_tag', 'events', array(
+    	register_taxonomy( 'event_tag', 'event', array(
             'labels' => [
                 'name' => _x( 'Tags', 'simple-event' ),
                 'singular_name' => _x( 'Tag', 'simple-event' ),
@@ -91,10 +91,10 @@ class Simple_Event {
                 'menu_name' => __( 'Tags' )
             ],	    		
 		 	'hierarchical' => false,
-			'query_var' => 'events_tag',
+			'query_var' => 'event_tag',
 			'rewrite' => [
 				'hierarchical' => true,
-				'slug' => 'events-tag',
+				'slug' => 'event-tag',
 				'with_front' => true,
 				'ep_mask' => EP_NONE,	
 			],
@@ -104,8 +104,92 @@ class Simple_Event {
 			) 
     	);
 
-	    register_taxonomy_for_object_type('events_category','events'); 
-	    register_taxonomy_for_object_type('events_tag','events');
+	    register_taxonomy_for_object_type('event_category','event'); 
+	    register_taxonomy_for_object_type('event_tag','event');
+
+	}
+
+	/**
+	 *
+	 *
+	 * @since 0.0.1
+	 */
+	public function events_meta_box_description_callback() {
+		global $post;
+		$cord = get_post_meta($post->ID,'event_location_latitude_longitude',true);
+		$date = get_post_meta($post->ID,'event_date',true);
+		$url = get_post_meta($post->ID,'event_url',true);
+		$addr = get_post_meta($post->ID,'event_location_address',true);
+		wp_nonce_field( 'simple_event_nonce', 'simple_event_nonce' );
+	?>
+        <div class="field wrapper" style="margin-bottom: 12px; overflow: auto;">
+            <div class="label" style="float: left; width: 25%; vertical-align: top">
+                <label for="event_date">Date</label><p class="description"></p>
+            </div>
+            <div class="input" style="float: left; width: 75%; vertical-align: top">
+                <input name="event_date" id="event_date" placeholder="" readonly autocomplete="off" value="<?=$date?>" class="large-text" type="text"><p class="description">Define the event date by selecting it from the jQuery DatePicker popup</p>
+            </div>
+        </div>
+        <div class="field wrapper" style="margin-bottom: 12px; overflow: auto;">
+            <div class="label" style="float: left; width: 25%; vertical-align: top">
+                <label for="event_location">Location</label><p class="description"></p>
+            </div>        
+            <div class="input" style="float: left; width: 75%; vertical-align: top">
+			    <div class="pac-card" id="pac-card">
+			      <div>
+			        <div id="title">
+			          Autocomplete search
+			        </div>
+			        <div id="type-selector" class="pac-controls">
+			          <input type="radio" name="type" id="changetype-all" checked="checked">
+			          <label for="changetype-all">All</label>
+
+			          <input type="radio" name="type" id="changetype-establishment">
+			          <label for="changetype-establishment">Establishments</label>
+
+			          <input type="radio" name="type" id="changetype-address">
+			          <label for="changetype-address">Addresses</label>
+
+			          <input type="radio" name="type" id="changetype-geocode">
+			          <label for="changetype-geocode">Geocodes</label>
+			        </div>
+			        <div id="strict-bounds-selector" class="pac-controls">
+			          <input type="checkbox" id="use-strict-bounds" value="">
+			          <label for="use-strict-bounds">Strict Bounds</label>
+			        </div>
+			      </div>
+			      <div id="pac-container">
+			      	<input type="hidden" name="event_location_latitude_longitude" value="<?=$cord?$cord:'-7.7974565,110.37069700000006'?>">
+			        <input id="pac-input" name="event_location_address" type="text" placeholder="Enter a location" value="<?=$addr?$addr:'Yogyakarta City, Special Region of Yogyakarta, Indonesia'?>">
+			      </div>
+			    </div>
+			    <div id="map"></div>
+			    <div id="infowindow-content">
+			      <img src="" width="16" height="16" id="place-icon">
+			      <span id="place-name"  class="title"></span><br>
+			      <span id="place-address"></span>
+			    </div>
+			    <p class="description">This field is integrated with Google places autocomplete and also the map </p>
+			</div>
+        </div>
+        <div class="field wrapper" style="margin-bottom: 12px; overflow: auto;">
+            <div class="label" style="float: left; width: 25%; vertical-align: top">
+                <label for="event_url">URL</label><p class="description"></p>
+            </div>
+            <div class="input" style="float: left; width: 75%; vertical-align: top">
+                <input name="event_url" id="event_url" placeholder="" value="<?=$url?>" class="large-text" type="url"><p class="description">Please make sure you fill the url starting with http:// or https:// </p>
+            </div>
+        </div>                
+	<?php        
+	}
+
+	/**
+	 * Create custom metabox
+	 *
+	 * @since 0.0.1
+	 */
+	public function create_the_metabox() {
+		add_meta_box( 'event-meta-box', 'Event Description', array($this,'events_meta_box_description_callback'), 'event', 'normal', 'high' );  
 	}
 
 	/**
@@ -114,11 +198,14 @@ class Simple_Event {
 	 * @since 0.0.1
 	 */
 	public static function flush_rewrite_rules() {
-		global $wp_rewrite;
-		if ( $wp_rewrite ) $wp_rewrite->flush_rules();
+		//global $wp_rewrite;
+		//if ( $wp_rewrite ) $wp_rewrite->flush_rules();
+		flush_rewrite_rules();
 	}
 
 	/**
+	 *
+	 *
 	 * @since 0.0.1
 	 *
 	 * @return object 
@@ -133,18 +220,21 @@ class Simple_Event {
      */
 	public function __construct() {
 
+		$this->init();
 		add_action('init', [$this, 'wp_init'] );
+		add_action('admin_init', [$this, 'wp_admin_init']);
 
-		static::$instance = $this;
 	}
 
 	/**
+	 * 
+	 *
 	 * Initializing class properties
 	 *
 	 * @since 0.0.1
 	 */
-	public function init() {
-		static::$instance = null;
+	protected function init() {
+		static::$instance = $this;
 	}
 
 	/**
@@ -153,8 +243,292 @@ class Simple_Event {
 	 * @since 0.0.1
 	 */
 	public function wp_init() {
-		$this->create_event_post_type();
+		$this->create_event_post_type();		
 	}
+
+	/**
+	 * 
+	 * 
+	 * @since 0.0.1
+	 */
+	public function wp_admin_init() {
+
+		global $pagenow, $hook_suffix, $typenow, $post;
+		if ( isset( $_GET['post'] ) )
+		 	$post_id = (int) $_GET['post'];
+		elseif ( isset( $_POST['post_ID'] ) )
+		 	$post_id = (int) $_POST['post_ID'];
+		else
+		 	$post_id = 0;		
+		if ( !($typenow == 'event' && $pagenow != 'edit.php' || ($pagenow == 'post.php' && $post_id > 0 && ($post = get_post($post_id)) && $post->post_type == 'event') ) ) return;
+
+
+	    add_action( 'add_meta_boxes', array($this,'create_the_metabox') );
+	    add_action( 'admin_enqueue_scripts', array($this, 'admin_enqueue_scripts') );
+	    add_action( 'admin_print_footer_scripts', array($this, 'admin_footer_js') );
+	    add_action( 'admin_print_scripts', array($this, 'admin_header_js'), 2, 1 );
+	    add_action( 'admin_print_styles', array($this, 'google_location_style') );
+
+        add_action('save_post', array($this,'save_event')); 
+        add_filter('pre_delete_post',array($this,'pre_delete_event'), 99, 3);	    
+	}
+
+	/**
+	 * 
+	 * 
+	 * @since 0.0.1
+	 */
+	public function admin_enqueue_scripts( $hook ) {
+		global $post;
+		wp_enqueue_script('jquery-ui-datepicker');
+		wp_enqueue_script('google-maps-js', '//maps.googleapis.com/maps/api/js?key=AIzaSyDPv3PPTE1PHXMmPejCmiPSIAVCGaJqlIE&libraries=places&callback=initMap', [], null, true);
+		wp_enqueue_style('jquery-ui-css','//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css',[],null);
+		$loc = get_post_meta($post->ID,'event_location_latitude_longitude',true);
+		if ( empty($loc) ) $loc = '-7.7974565, 110.37069700000006';
+		wp_localize_script('google-maps-js','eventLocation',explode(',', $loc));		
+	}
+
+	/**
+	 * 
+	 * 
+	 * @since 0.0.1
+	 */
+    public function save_event( $post_id ) {
+
+        if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
+        if( !isset( $_POST['simple_event_nonce'] ) || !wp_verify_nonce( $_POST['simple_event_nonce'], 'simple_event_nonce' ) ) return;
+        if( !current_user_can( 'edit_post' ) ) return;
+
+        $event_post = stripslashes_deep($_POST);
+
+        update_post_meta( $post_id, 'event_location_latitude_longitude', $event_post['event_location_latitude_longitude'] );
+        update_post_meta( $post_id, 'event_location_address', $event_post['event_location_address'] );
+        update_post_meta( $post_id, 'event_date', $event_post['event_date'] );
+        update_post_meta( $post_id, 'event_url', $event_post['event_url'] );
+    }
+
+	/**
+	 * 
+	 * 
+	 * @since 0.0.1
+	 */
+    public function pre_delete_event($chk, $post, $force_delete) {
+        if ( 'event' == $post->post_type ) {
+            delete_post_meta( $post->ID, 'event_location_latitude_longitude');    
+            delete_post_meta( $post->ID, 'event_location_address');  
+            delete_post_meta( $post->ID, 'event_date');       
+            delete_post_meta( $post->ID, 'event_url');                  
+        }
+        return $chk; // go with deletion
+    }
+
+	/**
+	 * 
+	 * 
+	 * @since 0.0.1
+	 */
+	public function google_location_style() {
+	?>
+	<style type="text/css">
+      div#event-meta-box #description {
+        font-family: Roboto;
+        font-size: 15px;
+        font-weight: 300;
+      }
+
+      div#event-meta-box #infowindow-content .title {
+        font-weight: bold;
+      }
+
+      div#event-meta-box #infowindow-content {
+        display: none;
+      }
+
+      div#event-meta-box #map #infowindow-content {
+        display: inline;
+      }
+
+      div#event-meta-box .pac-card {
+        margin: 10px 10px 0 0;
+        border-radius: 2px 0 0 2px;
+        box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        outline: none;
+        box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
+        background-color: #fff;
+        font-family: Roboto;
+      }
+
+      div#event-meta-box #pac-container {
+        padding-bottom: 12px;
+        margin-right: 12px;
+      }
+
+      div#event-meta-box .pac-controls {
+        display: inline-block;
+        padding: 5px 11px;
+      }
+
+      div#event-meta-box .pac-controls label {
+        font-family: Roboto;
+        font-size: 13px;
+        font-weight: 300;
+      }
+
+      div#event-meta-box #pac-input {
+        background-color: #fff;
+        font-family: Roboto;
+        font-size: 15px;
+        font-weight: 300;
+        margin-left: 12px;
+        padding: 0 11px 0 13px;
+        text-overflow: ellipsis;
+        width: 400px;
+      }
+
+      div#event-meta-box #pac-input:focus {
+        border-color: #4d90fe;
+      }
+
+      div#event-meta-box #title {
+        color: #fff;
+        background-color: #4d90fe;
+        font-size: 25px;
+        font-weight: 500;
+        padding: 6px 12px;
+      }
+    </style>
+	<?php
+	}
+
+	/**
+	 *
+	 *
+	 * @since 0.0.1
+	 */
+	public function admin_header_js() {
+	?>
+	  <script type="text/javascript">
+	  	/*
+	  	 * JavaScript code courtesy of Google Maps
+	  	 * https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete
+	  	 */
+	      function initMap() {
+	      	var mapDOM = document.getElementById('map');
+	      	if ( mapDOM ) {
+	      		mapDOM.style.width = '100%';
+	      		mapDOM.style.height = '350px';
+	      	}
+	        var map = new google.maps.Map(document.getElementById('map'), { 
+	          center: {lat: parseFloat(eventLocation[0]), lng: parseFloat(eventLocation[1])},
+	          zoom: 13
+	        });
+	        var card = document.getElementById('pac-card');
+	        var input = document.getElementById('pac-input');
+	        var types = document.getElementById('type-selector');
+	        var strictBounds = document.getElementById('strict-bounds-selector');
+
+	        map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
+
+	        var autocomplete = new google.maps.places.Autocomplete(input);
+
+	        // Bind the map's bounds (viewport) property to the autocomplete object,
+	        // so that the autocomplete requests use the current map bounds for the
+	        // bounds option in the request.
+	        autocomplete.bindTo('bounds', map);
+
+	        // Set the data fields to return when the user selects a place.
+	        autocomplete.setFields(
+	            ['address_components', 'geometry', 'icon', 'name']);
+
+	        var infowindow = new google.maps.InfoWindow();
+	        var infowindowContent = document.getElementById('infowindow-content');
+	        infowindow.setContent(infowindowContent);
+	        var marker = new google.maps.Marker({
+	          map: map,
+	          anchorPoint: new google.maps.Point(0, -29)
+	        });
+
+	        marker.setPosition({lat: parseFloat(eventLocation[0]), lng: parseFloat(eventLocation[1])});
+	        
+	        autocomplete.addListener('place_changed', function() {
+	          infowindow.close();
+	          marker.setVisible(false);
+	          var place = autocomplete.getPlace();
+	          document.getElementsByName('event_location_latitude_longitude')[0].value = place.geometry.location.lat().toString()+','+place.geometry.location.lng().toString();
+	          if (!place.geometry) {
+	            // User entered the name of a Place that was not suggested and
+	            // pressed the Enter key, or the Place Details request failed.
+	            window.alert("No details available for input: '" + place.name + "'");
+	            return;
+	          }
+
+	          // If the place has a geometry, then present it on a map.
+	          if (place.geometry.viewport) {
+	            map.fitBounds(place.geometry.viewport);
+	          } else {
+	            map.setCenter(place.geometry.location);
+	            map.setZoom(17); 
+	          }
+	          marker.setPosition(place.geometry.location);
+	          marker.setVisible(true);
+
+	          var address = '';
+	          if (place.address_components) {
+	            address = [
+	              (place.address_components[0] && place.address_components[0].short_name || ''),
+	              (place.address_components[1] && place.address_components[1].short_name || ''),
+	              (place.address_components[2] && place.address_components[2].short_name || '')
+	            ].join(' ');
+	          }
+
+	          infowindowContent.children['place-icon'].src = place.icon;
+	          infowindowContent.children['place-name'].textContent = place.name;
+	          infowindowContent.children['place-address'].textContent = address;
+	          infowindow.open(map, marker);
+	        });
+
+	        // Sets a listener on a radio button to change the filter type on Places
+	        // Autocomplete.
+	        function setupClickListener(id, types) {
+	          var radioButton = document.getElementById(id);
+	          radioButton.addEventListener('click', function() {
+	            autocomplete.setTypes(types);
+	          });
+	        }
+
+	        setupClickListener('changetype-all', []);
+	        setupClickListener('changetype-address', ['address']);
+	        setupClickListener('changetype-establishment', ['establishment']);
+	        setupClickListener('changetype-geocode', ['geocode']);
+
+	        document.getElementById('use-strict-bounds')
+	            .addEventListener('click', function() {
+	              console.log('Checkbox clicked! New state=' + this.checked);
+	              autocomplete.setOptions({strictBounds: this.checked});
+	            });
+	      }		  
+	  </script>    
+	<?php
+	}
+
+	/**
+	 * 
+	 * 
+	 * @since 0.0.1
+	 */
+	public function admin_footer_js( ) {
+	?>
+		<script type="text/javascript">
+		  ( function($) {
+		  	$(window).load(function() {
+				$( "#event_date" ).datepicker();
+				initMap();
+		  	});		    
+		  } )(jQuery);
+		</script>
+	<?php
+	}	
 
 	/**
 	 * 
@@ -166,7 +540,7 @@ class Simple_Event {
 	}
 
 	/**
-	 *
+	 * 
 	 *
 	 * @since 0.0.1
 	 */
